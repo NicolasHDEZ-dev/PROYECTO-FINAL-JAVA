e6073897a26d8a33a8b28b9839e37e90625643eb
Nics <nicsmusiccontact@gmail.com>
2023-01-29 12:01:16 -0300

    update 22

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-const comprar = document.querySelectorAll(".agregaralcarrito")
-
-comprar.forEach((botones)=> botones.addEventListener('click', ()=>{
-    botones.innerText= "EN EL CARRITO"
-}))
-
-
 
 const titulo = document.getElementById("titulo")
 
@@ -21,6 +14,7 @@ const clave = document.getElementById("clave")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
 const btnSubmit = document.getElementById("btnSubmit")
+let BotonesComprar = document.querySelectorAll(".agregaralcarrito")
 
 form.addEventListener('submit', e=>{
     e.preventDefault()
@@ -53,3 +47,78 @@ form.addEventListener('submit', (e)=>{
 const stockNike = ["Nike Uptempo", "Nike Airmax 1"]
 const stockJordan = ["Jordan 1 Fragment", "Jordan 1 Black Toe", "Jordan 11 Concord", "Jordan 4 Bred"]
 const stockTotal = [...stockNike, ...stockJordan]
+
+const tarjetas = document.querySelector(".tarjetas")
+
+const renderCards = (arr) => {
+    let html ;
+    for(const item of arr){
+        const {id, nombre,img,precio,talle} = item;
+
+        html = `<div class="col-12 col-md-6">
+        <div class="item shadow mb-4">
+            <h3 class="item-title text-center">${nombre}</h3>
+            <img class="imagen" src="${img}">
+
+            <div class="item-details ">
+                <h4 class="item-price">${precio}$</h4>
+                <h4 class="item-price">Talle ${talle}</h4>
+                
+            </div>
+            <div class=" text-center">
+            <button class="btn btn-danger agregaralcarrito btnAdd" id=${id}>AÑADIR AL CARRITO</button>
+            </div>
+        </div>
+    </div>`;
+
+    tarjetas.innerHTML += html;
+
+    actualizarBotonesComprar();   
+    }
+}
+
+fetch("./data/data.json")
+.then(res=>res.json())
+.then(data=>{
+    productos = data;
+    renderCards(productos)
+})
+
+function actualizarBotonesComprar() {
+   BotonesComprar = document.querySelectorAll(".agregaralcarrito")
+
+   BotonesComprar.forEach(botones => {
+    botones.addEventListener("click", ()=>{
+        botones.innerText = "EN EL CARRITO"
+        botones.classList.add("disabled")
+    })
+    botones.addEventListener("click", agregarAlCarrito)
+   });
+};
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productosdelcarrito")
+
+if(productosEnCarritoLS){
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+} else {
+ productosEnCarrito = [];
+}
+
+function agregarAlCarrito (e){
+const idBoton = e.currentTarget.id
+
+const productoAgregado = productos.find(item => item.id === idBoton)
+productosEnCarrito.push(productoAgregado)
+
+
+console.log((productosEnCarrito.includes(productoAgregado)))
+ 
+
+localStorage.setItem("productosdelcarrito", JSON.stringify(productosEnCarrito))
+}
+
+if(productosEnCarritoLS === productosEnCarritoLS){
+    actualizarBotonesComprar()
+}
